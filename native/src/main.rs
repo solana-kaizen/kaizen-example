4b8be2cb2344a4947c7c2e069b69bb459123b428
Anton Yemelyanov <anton.yemelyanov@gmail.com>
2022-11-02 05:35:31 +0000

    cleanup

--- native/src/main.rs
+++ native/src/main.rs
@@ -7,16 +7,8 @@ use tokio;
 
 async fn main_impl()->Result<()>{
 
-    let emulator_host = "rpc://127.0.0.1:9393";
-    let test_validator_host = "http://127.0.0.1:8899";
-
-    let emulator_mode = false;
-
-    let network = if emulator_mode {
-        emulator_host
-    }else{
-        test_validator_host
-    };
+    let network = "rpc://127.0.0.1:9393";
+    // let network = "http://127.0.0.1:8899";
 
     let transport = Transport::try_new(
         network,
@@ -24,20 +16,15 @@ async fn main_impl()->Result<()>{
     ).await?;
     transport.connect(true).await?;
 
-    transport.set_custom_authority(
-        Some(Pubkey::from_str("42bML5qB3WkMwfa2cosypjUrN7F2PLQm4qhxBdRDyW7f")?)
-    )?;
-
-    if emulator_mode{
-        let authority = transport.get_authority_pubkey()?;
-
-        transport
-        .emulator()
-            .expect("Missing emulator interface")
-            .fund(
-                &authority,
-                &Pubkey::default(),
-                utils::sol_to_lamports(500.0)
+    if let Some(emulator) = transport.emulator() {
+        let authority = Pubkey::from_str("42bML5qB3WkMwfa2cosypjUrN7F2PLQm4qhxBdRDyW7f")?;
+        transport.set_custom_authority(
+            Some(authority)
+        )?;
+        emulator.fund(
+            &authority,
+            &Pubkey::default(),
+            utils::sol_to_lamports(500.0)
         ).await?;
     }
 
