a5df6727c40e2baeb4eee94568cbd1e09edc6a78
Anton Yemelyanov <anton.yemelyanov@gmail.com>
2023-02-04 17:21:13 -0500

    update dependencies to use the workspace

--- simulator/src/main.rs
+++ simulator/src/main.rs
@@ -16,7 +16,7 @@ cfg_if! {
         #[derive(Debug, Error)]
         enum Error {
             #[error("WebSocket error: {0}")]
-            WebSocket(#[from] workflow_websocket::server::Error),
+            WebSocket(#[from] WebSocketError),
             #[error("Workflow allocator error: {0}")]
             WorkflowAllocator(String),
             #[error("I/O error: {0}")]
